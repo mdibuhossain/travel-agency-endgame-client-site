3af3362b7399f7f51bcfa54aa42a12d3ff7a84da
Ibrahim H <mohammad.ihakash@gmail.com>
2022-02-05 03:06:37 +0600

    avatar editor setup

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ import SingleBlog from './Page/SingleBlog';
 import UpdateService from './Page/UpdateService';
 import AdminRoute from './PrivateRoute/AdminRoute';
 import PrivateRoute from './PrivateRoute/PrivateRoute';
-import "@material-tailwind/react/tailwind.css";
 
 function App() {
   return (
